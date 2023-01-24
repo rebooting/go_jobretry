6c56eb0678884e5064fb3ac2f5733803072268b7
Alvin <rebooting@gmail.com>
2023-01-24 11:48:23 +0000

    added test case

--- queue.go
+++ queue.go
@@ -1,6 +1,70 @@
 package go_diskqueue
 
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
-func Queue(){
-	
+const (
+	PENDING = iota
+	DONE
+	QUEUE
+)
+
+const (
+	STATE_QUEUE   = ".queue"
+	STATE_PENDING = ".pending"
+	STATE_DONE    = ".done"
+)
+
+type Queue struct {
+}
+
+type QueueExtensions struct {
+	stage string
+	count int
+}
+
+func New() Queue {
+	return Queue{}
+}
+
+type NotFileError struct{}
+
+func (m NotFileError) Error() string {
+	return "Not a file"
+}
+
+type InvalidQueueFormatError struct{}
+
+func (m InvalidQueueFormatError) Error() string {
+	return "Not a queue file"
+}
+
+func InsertIntoQueue(filename string, queueName string) error {
+	return nil
+}
+
+func getExtensions(filename string) error {
+	cleanPath := filepath.Clean(filename)
+	//check if filename is a filename
+	fileInfo, err := os.Stat(filename)
+
+	if err != nil {
+		log.Printf("error when Stat %s", err.Error())
+		return err
+	}
+
+	if fileInfo.IsDir() {
+		return NotFileError{}
+	}
+	fileStr := filepath.Base(cleanPath)
+	fileParts := strings.Split(fileStr, ".")
+	if len(fileParts) < 3 {
+		return InvalidQueueFormatError{}
+	}
+	return nil
+
 }
--- queue_test.go
+++ queue_test.go
@@ -1,8 +1,57 @@
-package go_diskqueue_test
+package go_diskqueue
 
-import("testing")
+import (
+	"errors"
+	"log"
+	"os"
+	"testing"
+)
 
+// setup files for test cases
+func createTestFile(t *testing.T, fileName string) {
+	f, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	defer f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
-func TestQueue(t *testing.T){
-	
+func removeTestFile(t *testing.T, filename string) {
+	t.Logf("removing. .. %s\n", filename)
+	e := os.Remove(filename)
+	if e != nil {
+		t.Logf("trying to remove file in cleanup, error %v", e)
+	}
+}
+
+func TestInsertIntoQueue(t *testing.T) {
+	createTestFile(t, "/tmp/dummy.txt")
+	removeTestFile(t, "/tmp/dummy.txt")
+}
+
+func TestGetExtension(t *testing.T) {
+	type TestCase struct {
+		Filename    string
+		ExpectedErr error
+	}
+
+	tc := []TestCase{
+		{
+			Filename:    "jello.001.pending",
+			ExpectedErr: nil,
+		},
+		{
+			Filename:    "jello.pending",
+			ExpectedErr: InvalidQueueFormatError{},
+		},
+	}
+
+	for _, eachCase := range tc {
+		createTestFile(t,eachCase.Filename)
+		defer removeTestFile(t,eachCase.Filename)
+		err := getExtensions(eachCase.Filename)
+		if !errors.Is(err, eachCase.ExpectedErr) {
+			t.Errorf("expected %v, got %v", eachCase.ExpectedErr, err)
+		}
+	}
 }
